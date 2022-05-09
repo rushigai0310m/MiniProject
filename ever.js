0ae23680dda784061a28d8bb7fb657ae23bd5741
unknown <Rushikesh>
2022-05-09 13:38:13 +0530

    Done With Project

---
+++ ever.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){  
+// 	  $("#firstimg").click(function(){
+
+//     firstimg.animate({height: '300px', opacity: '0.4'}, "slow");
+//     div.animate({width: '300px', opacity: '0.8'}, "slow");
+//     div.animate({height: '100px', opacity: '0.4'}, "slow");
+//     div.animate({width: '100px', opacity: '0.8'}, "slow");
+//   });  
+// }); 
+
